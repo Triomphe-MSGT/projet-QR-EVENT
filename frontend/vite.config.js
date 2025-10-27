7ebbcdd5f1bcd1cfe7abfd1dc842da19b9023949
triomphe <gregoiremandeng@gmail.com>
2025-10-27 09:57:35 +0100

    verification du json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   build: {
-    chunkSizeWarningLimit: 2000, // en kB
+    chunkSizeWarningLimit: 2000,
   },
 });
