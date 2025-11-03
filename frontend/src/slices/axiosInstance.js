fc483e2929cef1e8b653e19a0a49250b8480acf9
triomphe <gregoiremandeng@gmail.com>
2025-11-03 21:28:46 +0100

    connection backend

--- frontend/src/slices/axiosInstance.js
+++ frontend/src/slices/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3001/api";
+  import.meta.env.VITE_API_BASE_URL ||
+  ";https://projet-qr-event-uzrp.onrender.com/api";
 const api = axios.create({
   baseURL: API_BASE_URL,
 
